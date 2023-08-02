f8ccb382dd249d49e56558fbd8f2da110dbfd751
Soumya <soumyars675@gmail.com>
2023-08-02 12:07:17 +0530

    Again update8

--- src/screens/LoginForm.js
+++ src/screens/LoginForm.js
@@ -69,7 +69,7 @@ const LoginForm = ({ navigation }) => {
         placeholderTextColor="black"
         style={{ borderColor: 'black', borderWidth: 1, color: 'black' }}
       />
-      <Text>Welcome To The App</Text>
+      <Text>Welcome</Text>
       <Button title="Login" onPress={handleLogin} testID='login'/>
       {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
     </View>

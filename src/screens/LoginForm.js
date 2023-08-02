b56daa9ae58bd34da5261133606c2eac2d5b1b57
Soumya <soumyars675@gmail.com>
2023-08-02 12:28:24 +0530

    Again update10

--- src/screens/LoginForm.js
+++ src/screens/LoginForm.js
@@ -1,12 +1,19 @@
 // LoginForm.js
 
-import React, {useState,useEffect} from 'react';
-import {View, TextInput, Button, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, TextInput, Button, Text, FlatList} from 'react-native';
 import {validateNullOrEmpty, validateEmail} from '../utils/ValidationUtil';
 import {CustomError} from '../utils/ErrorHandlingUtil';
 import axios from 'axios';
 
-const LoginForm = ({ navigation }) => {
+const Item = ({item}) => (
+  <View style={{margin: 10}}>
+    <Text style={{color: 'black'}}>{item.email}</Text>
+    <Text style={{color: 'black'}}>{item.password}</Text>
+  </View>
+);
+
+const LoginForm = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -44,7 +51,7 @@ const LoginForm = ({ navigation }) => {
         throw new CustomError('No users found', 'login');
       } else if (data.some(element => element.email === email)) {
         throw new CustomError('Incorrect password', 'login');
-      } else if(!matchedUser) {
+      } else if (!matchedUser) {
         throw new CustomError('User does not exist', 'login');
       }
     } catch (error) {
@@ -54,12 +61,15 @@ const LoginForm = ({ navigation }) => {
 
   return (
     <View>
+      <Text style={{color: 'blue', fontSize: 35, fontWeight: 'bold'}}>
+        Welcome To App Mode
+      </Text>
       <TextInput
         placeholder="Email"
         value={email}
         onChangeText={text => setEmail(text)}
         placeholderTextColor="black"
-        style={{ borderColor: 'black', borderWidth: 1, color: 'black' }}
+        style={{borderColor: 'black', borderWidth: 1, color: 'black'}}
       />
       <TextInput
         placeholder="Password"
@@ -67,11 +77,17 @@ const LoginForm = ({ navigation }) => {
         onChangeText={text => setPassword(text)}
         secureTextEntry
         placeholderTextColor="black"
-        style={{ borderColor: 'black', borderWidth: 1, color: 'black' }}
+        style={{borderColor: 'black', borderWidth: 1, color: 'black'}}
       />
-      <Text>Welcome To App Mode</Text>
-      <Button title="Login" onPress={handleLogin} testID='login'/>
-      {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
+      <Button title="Login" onPress={handleLogin} testID="login" />
+      {data.length !== 0 && (
+        <FlatList
+          data={data}
+          renderItem={({item}) => <Item item={item} />}
+          keyExtractor={item => item.id}
+        />
+      )}
+      {error ? <Text style={{color: 'red'}}>{error}</Text> : null}
     </View>
   );
 };
